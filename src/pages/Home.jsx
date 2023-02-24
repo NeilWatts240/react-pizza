7ac868709ea7103b8421d0c536dfc971426d66a9
NeilWatts240 <vasilenko.m96@gmail.com>
2023-02-24 20:40:58 +0300

    commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import Pagination from "../components/Pagination";
+
+function Home({ searchValue }) {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsloading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [sortType, setSortType] = React.useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
+
+  React.useEffect(() => {
+    setIsloading(true);
+
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
+
+    fetch(
+      `https://63d9a09f2af48a60a7bd2407.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+    )
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsloading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
+
+  return (
+    <div className="container">
+      <div className="container">
+        <div className="content__top">
+          <Categories
+            value={categoryId}
+            onChangeCategory={(i) => setCategoryId(i)}
+          />
+          <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+        <Pagination onChangePage={(number) => setCurrentPage(number)} />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
